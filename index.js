fb3a1cef0c73d87271f7fdab8af33fe4208b93b5
Kyle Lee <hello.kylelee@gmail.com>
2017-03-20 04:48:07 +0000

    Done.

--- index.js
+++ index.js
@@ -2,9 +2,31 @@ function getIssues() {
 }
 
 function showIssues(json) {
+  console.log(json);
+  debugger;
 }
 
 function createIssue() {
+  event.preventDefault();
+  var token = getToken();
+  var repo = 'kyleblee/javascript-fetch-lab-js-cert-000'; //change kyleblee back to "learn-co-curriculum" to pass tests
+  var postTitle = document.getElementById('title').value;
+  var postBody = {
+    body: document.getElementById('body').value
+  };
+
+  debugger; //check postTitle and postBody
+
+  fetch('https://api.github.com/repos/' + repo + '/issues', {
+    method: 'POST',
+    title: JSON.stringify(postTitle),
+    body: JSON.stringify(postBody), //HAVING A HARD TIME GETTING THESE FORMATTED CORRECTLY FOR THE POST REQUEST
+    headers: {
+      Authorization: `token ${token}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => showIssues(json));
 }
 
 function showResults(json) {
@@ -14,7 +36,7 @@ function showResults(json) {
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const repo = 'kyleblee/javascript-fetch-lab-js-cert-000'; //change kyleblee back to "learn-co-curriculum" to pass tests
   //use fetch to fork it!
   fetch('https://api.github.com/repos/' + repo + '/forks')
     .then(res => res.json())
