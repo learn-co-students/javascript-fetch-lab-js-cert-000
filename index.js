87aeab1089686818142c6f9ba6eab1a9addd76df
Kyle Lee <hello.kylelee@gmail.com>
2017-03-21 02:09:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
+const username = 'kyleblee';
+const baseAPI = 'https://api.github.com/';
+const fork = `${username}/javascript-fetch-lab`;
+
 function getIssues() {
+  fetch(`${baseAPI}repos/${fork}/issues`).
+    then(res => res.json()).
+    then(json => showIssues(json))
 }
 
-function showIssues(el) {
-  //getting the updated list of issues
-  const repo = 'learn-co-curriculum/javascript-fetch-lab-js-cert-000'; //change kyleblee back to "learn-co-curriculum" to pass tests
-  fetch('https://api.github.com/repos/' + repo + '/issues')
-    .then(res => res.json())
-    .then(json => displayIssues(json))
-
-  function displayIssues(json) {
-    var issuesTemplate = Handlebars.compile(document.getElementById('issues-template').innerHTML);
-    var issuesList = issuesTemplate(json);
-    debugger; //check what issuesList looks like
-    document.getElementById('issues').innerHTML = issuesList;
-  }
+function showIssues(json) {
+  var issuesTemplate = Handlebars.compile(document.getElementById('issues-template').innerHTML);
+  document.getElementById('issues').innerHTML = issuesTemplate(json);
 }
 
 function createIssue() {
-  event.preventDefault();
-  var token = getToken();
-  var repo = 'learn-co-curriculum/javascript-fetch-lab-js-cert-000'; //change kyleblee back to "learn-co-curriculum" to pass tests
+  // event.preventDefault();
   var postTitle = document.getElementById('title').value;
   var postBody = document.getElementById('body').value;
   var postData = {
@@ -27,29 +22,30 @@ function createIssue() {
     body: postBody
   }
 
-  fetch('https://api.github.com/repos/' + repo + '/issues', {
-    method: 'POST',
+  fetch(`${baseAPI}repos/${fork}/issues`, {
+    method: 'post',
     body: JSON.stringify(postData),
     headers: {
-      Authorization: `token ${token}`
+      Authorization: `token ${getToken()}`
     }
-  })
-    .then(res => res.json())
-    .then(json => showIssues(json));
+  }).then(res => getIssues());
 }
 
 function showResults(json) {
   var resultsTemplate = Handlebars.compile(document.getElementById('repo-template').innerHTML);
-  var resultsList = resultsTemplate(json);
-  document.getElementById('results').innerHTML = resultsList;
+  document.getElementById('results').innerHTML = resultsTemplate(json);
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab-js-cert-000'; //change kyleblee back to "learn-co-curriculum" to pass tests
-  //use fetch to fork it!
-  fetch('https://api.github.com/repos/' + repo + '/forks')
-    .then(res => res.json())
-    .then(json => showResults(json));
+  const repo = 'learn-co-curriculum/javascript-fetch-lab';
+
+  fetch(`${baseAPI}repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).
+    then(json => showResults(json));
 }
 
 function getToken() {
